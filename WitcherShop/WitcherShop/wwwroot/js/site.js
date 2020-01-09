43b7995d91f3ac6f6f3e9a80631937ca7e8da29d
AndrewK-ua <54717190+AndrewK-ua@users.noreply.github.com>
2020-01-09 19:46:23 +0200

    ...

---
+++ WitcherShop/WitcherShop/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+//can be implemented with :hover
+function mouseOverPicture(target) {
+    console.log(target.tagName);
+    target.setAttribute('style', 'opacity: 50%;');
+    let button = document.getElementById('button-add1');
+    button.setAttribute('style', 'display: block; z-index: 5; user-select: all');
+    console.log('onmouseover');
+    return target;
+}
+
+function mouseOutPicture(target) {
+    console.log(target.tagName);
+    target.setAttribute('style', 'opacity: 100%;');
+    let button = document.getElementById('button-add1');
+    button.setAttribute('style', 'display: none; z-index: 1; user-select: none');
+    console.log('onmouseout');
+    return target;
+}
